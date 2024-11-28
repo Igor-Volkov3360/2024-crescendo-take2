3b06246d61d6e55ac886f34b5207f5f3af413b7d
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2024-11-28 18:12:53 -0500

    forgot to put actual changes

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,1 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+// * */
+public class ExampleTest {
+
+  @Test
+  /**
+   * this is an example test method. The goal of a test method is to test if the output of a piece
+   * of code matches expectanctions - Some useful guidelines for testing
+   *
+   * <p>Do test if the expected output of a method matches the actual one.
+   *
+   * <p>Do test whether the function calls made inside the method are occurring the desired number
+   * of times.
+   *
+   * <p>Do not try to test code that is not a part of the method under test.
+   *
+   * <p>Do not make API calls, database connections, or network requests while writing your tests.
+   */
+  public void dummyTest() {
+    // this is one the many methods you can use for testing the output of classes or methods
+    assertEquals(1, 1);
+  }
+}
